9f0b45745932e1675cdb839db64210c4dbe14a94
aviv.daniel <aviv.daniel@zoominfo.com>
2023-12-26 13:08:29 +0200

    :tada: Get Task By ID

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,20 +1,26 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { TasksService } from './tasks.service';
+import {Body, Controller, Get, Param, Post} from '@nestjs/common';
+import {TasksService} from './tasks.service';
 import {CreateTaskDto} from "./dto/create-task.dto";
 
 @Controller('tasks')
 export class TasksController {
-  constructor(private tasksService: TasksService) {}
+    constructor(private tasksService: TasksService) {
+    }
 
-  @Get()
-  getAllTasks() {
-    return this.tasksService.getAllTasks();
-  }
+    @Get()
+    getAllTasks() {
+        return this.tasksService.getAllTasks();
+    }
 
-  @Post()
-  createTask(
-    @Body() createTaskDto: CreateTaskDto
-  ) {
-    return this.tasksService.createTask(createTaskDto);
-  }
+    @Get('/:id')
+    getTaskById(@Param('id') id: string) {
+       return this.tasksService.getTaskById(id)
+    }
+
+    @Post()
+    createTask(
+        @Body() createTaskDto: CreateTaskDto
+    ) {
+        return this.tasksService.createTask(createTaskDto);
+    }
 }
